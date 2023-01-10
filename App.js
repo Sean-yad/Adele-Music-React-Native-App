a5a788d06f59cd507c7afc913af1012b3dc18c9b
Sean-yad <94640179+Sean-yad@users.noreply.github.com>
2023-01-10 12:40:38 -0600

    Update App.js

--- App.js
+++ App.js
@@ -1,26 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './screens/LoginScreen';
-import HomeScreen from './screens/HomeScreen';
-import MainContainer from './screens/MainContainer';
-import Live from './screens/Live';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./screens/LoginScreen";
+import HomeScreen from "./screens/HomeScreen";
+import MainContainer from "./screens/MainContainer";
+import Live from "./screens/Live";
 
 const Stack = createNativeStackNavigator();
-function Home(){
-  return(
-    <MainContainer/>
-  )
+function Home() {
+  return <MainContainer />;
 }
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen options={{ headerShown: false }} name="Login" component={LoginScreen} />
-        <Stack.Screen options={{ headerShown: false }} name="Home" component={MainContainer} />
-       
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Home"
+          component={MainContainer}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -29,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
